40ba95f76b9a34a770d96ecd65bddd1245974392
Ashwin <ashwin.sagodira@epitech.eu>
2023-02-24 16:15:29 +0100

    [ADD] step 5

--- gateaway/index.js
+++ gateaway/index.js
@@ -1,8 +1,23 @@
 const express = require('express')
 const helmet = require("helmet");
+const cors = require("cors");
+const { Router } = require('express');
 const app = express()
 const port = 3000
 
+app.use(helmet());
+app.use(cors());
+
+app.all('*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  res.header('Acess-Control-Allow-Headers, Content-Type');
+});
+
+Router.all('/:Apiname/*', (req, res, next) => {
+  req.params.Apiname = req.params.Apiname.toLowerCase();
+
+});
 
 app.get('/', (req, res) => {
   res.send('Home gateway !')
